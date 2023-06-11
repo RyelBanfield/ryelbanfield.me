a1babdb41f8c4bd2384a44fe815a46e4aec8db09
Ryel Banfield <ryelbanfield@gmail.com>
2023-06-11 14:38:57 -0400

    Remove projects

--- src/projects.ts
+++ src/projects.ts
@@ -1,6 +1,6 @@
 import QLinkPreview from "./images/screenshots/qlink/QLink-3.png";
-import ViewFinderPreview from "./images/screenshots/view-finder/ViewFinder-3.png";
-import StoryAnalyticsPreview from "./images/screenshots/story-analytics/StoryAnalytics-1.png";
+// import ViewFinderPreview from "./images/screenshots/view-finder/ViewFinder-3.png";
+// import StoryAnalyticsPreview from "./images/screenshots/story-analytics/StoryAnalytics-1.png";
 import F1RacingNowPreview from "./images/screenshots/f1-racing-now/F1RacingNow-1.png";
 
 import QLinkImage_1 from "./images/screenshots/qlink/QLink-1.png";
@@ -8,12 +8,12 @@ import QLinkImage_2 from "./images/screenshots/qlink/QLink-2.png";
 import QLinkImage_3 from "./images/screenshots/qlink/QLink-3.png";
 import QLinkImage_4 from "./images/screenshots/qlink/QLink-4.png";
 
-import ViewFinder_1 from "./images/screenshots/view-finder/ViewFinder-1.png";
-import ViewFinder_2 from "./images/screenshots/view-finder/ViewFinder-2.png";
-import ViewFinder_3 from "./images/screenshots/view-finder/ViewFinder-3.png";
-import ViewFinder_4 from "./images/screenshots/view-finder/ViewFinder-4.png";
+// import ViewFinder_1 from "./images/screenshots/view-finder/ViewFinder-1.png";
+// import ViewFinder_2 from "./images/screenshots/view-finder/ViewFinder-2.png";
+// import ViewFinder_3 from "./images/screenshots/view-finder/ViewFinder-3.png";
+// import ViewFinder_4 from "./images/screenshots/view-finder/ViewFinder-4.png";
 
-import StoryAnalytics_1 from "./images/screenshots/story-analytics/StoryAnalytics-1.png";
+// import StoryAnalytics_1 from "./images/screenshots/story-analytics/StoryAnalytics-1.png";
 
 import F1RacingNow_1 from "./images/screenshots/f1-racing-now/F1RacingNow-1.png";
 import F1RacingNow_2 from "./images/screenshots/f1-racing-now/F1RacingNow-2.png";
@@ -22,7 +22,6 @@ import F1RacingNow_4 from "./images/screenshots/f1-racing-now/F1RacingNow-4.png"
 
 export const projects = [
   {
-    development: true,
     slug: "qlink",
     name: "QLink",
     shortDescription: "QR Code Generator & Link Host",
@@ -42,47 +41,46 @@ export const projects = [
     githubRepository: "https://github.com/RyelBanfield/qlink",
     liveLink: "https://qlink.tech",
   },
+  // {
+  //   development: true,
+  //   slug: "view-finder",
+  //   name: "View Finder",
+  //   shortDescription: "Event Image Sharing Platform",
+  //   longDescription:
+  //     "An event image sharing platform allowing photographers to securely and easily upload and share their event photos with attendees in one place. The platform will offer user management, customizable galleries, image downloads, a user-friendly interface, and affordable pricing, making it a convenient and accessible solution for photographers and attendees alike. The platform is powered by Firebase for its backend and is available as both a web app built with Next.js and a React Native app.",
+  //   preview: ViewFinderPreview,
+  //   stack: [
+  //     "TypeScript",
+  //     "Next.js",
+  //     "TailwindCSS",
+  //     "React Native",
+  //     "Firebase",
+  //     "Vercel",
+  //   ],
+  //   screenshots: [ViewFinder_1, ViewFinder_2, ViewFinder_3, ViewFinder_4],
+  //   githubRepository: "https://github.com/RyelBanfield/viewfinder-web",
+  //   liveLink: "https://viewfinder-web.vercel.app",
+  // },
+  // {
+  //   development: true,
+  //   slug: "story-analytics",
+  //   name: "Story Analytics",
+  //   shortDescription: "-",
+  //   longDescription: "The scope of this project is being reconsidered.",
+  //   preview: StoryAnalyticsPreview,
+  //   stack: [
+  //     "TypeScript",
+  //     "Next.js",
+  //     "TailwindCSS",
+  //     "Prisma",
+  //     "NextAuth",
+  //     "Vercel",
+  //   ],
+  //   screenshots: [StoryAnalytics_1],
+  //   githubRepository: "https://github.com/RyelBanfield/story-analytics",
+  //   liveLink: "https://storyanalytics.tech",
+  // },
   {
-    development: true,
-    slug: "view-finder",
-    name: "View Finder",
-    shortDescription: "Event Image Sharing Platform",
-    longDescription:
-      "An event image sharing platform allowing photographers to securely and easily upload and share their event photos with attendees in one place. The platform will offer user management, customizable galleries, image downloads, a user-friendly interface, and affordable pricing, making it a convenient and accessible solution for photographers and attendees alike. The platform is powered by Firebase for its backend and is available as both a web app built with Next.js and a React Native app.",
-    preview: ViewFinderPreview,
-    stack: [
-      "TypeScript",
-      "Next.js",
-      "TailwindCSS",
-      "React Native",
-      "Firebase",
-      "Vercel",
-    ],
-    screenshots: [ViewFinder_1, ViewFinder_2, ViewFinder_3, ViewFinder_4],
-    githubRepository: "https://github.com/RyelBanfield/viewfinder-web",
-    liveLink: "https://viewfinder-web.vercel.app",
-  },
-  {
-    development: true,
-    slug: "story-analytics",
-    name: "Story Analytics",
-    shortDescription: "-",
-    longDescription: "The scope of this project is being reconsidered.",
-    preview: StoryAnalyticsPreview,
-    stack: [
-      "TypeScript",
-      "Next.js",
-      "TailwindCSS",
-      "Prisma",
-      "NextAuth",
-      "Vercel",
-    ],
-    screenshots: [StoryAnalytics_1],
-    githubRepository: "https://github.com/RyelBanfield/story-analytics",
-    liveLink: "https://storyanalytics.tech",
-  },
-  {
-    development: false,
     slug: "f1-racing-now",
     name: "F1 Racing Now",
     shortDescription: "Current F1 Season Points & Schedule",
