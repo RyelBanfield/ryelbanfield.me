44220505e9e2cb981364d7c7f6d01d9dff9b5ab3
Ryel Banfield <ryelbanfield@gmail.com>
2023-06-11 02:24:58 -0400

    Website refresh

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,23 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      animation: {
-        text: "text 5s ease infinite",
-      },
-      keyframes: {
-        text: {
-          "0%, 100%": {
-            "background-size": "200% 200%",
-            "background-position": "left center",
-          },
-          "50%": {
-            "background-size": "200% 200%",
-            "background-position": "right center",
-          },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
