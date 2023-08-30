a0727014b9f03649c49cd2e5881562a35341ae46
Ryel Banfield <ryelbanfield@gmail.com>
2023-08-30 19:23:48 -0400

    Update to Astro V3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,13 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), react(), mdx(), prefetch()]
+    site: 'https://ryelbanfield.me',
+  integrations: [react(), tailwind(), mdx(), prefetch(), sitemap()]
 });
