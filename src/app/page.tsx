71a06450763f071a550abccd7b2b5cb023bfeb89
Ryel Banfield <ryelbanfield@gmail.com>
2024-05-18 17:30:53 -0400

    Add illustrations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
+import Image from "next/image";
+
+import HeroSVG from "@/assets/undraw_building_websites_i78t.svg";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div className="px-5">
-      <nav className="flex items-center justify-between py-5">
+    <div className="">
+      <nav className="fixed top-0 flex w-full items-center justify-between  bg-neutral-200 p-5 text-neutral-800 dark:bg-neutral-950 dark:text-neutral-400">
         <p className="text-sm">ryelbanfield.me</p>
 
         <ThemeSwitcher />
       </nav>
 
-      <main>
-        <div className="flex flex-col gap-5 py-5">
+      <main className="mt-20">
+        <div className="flex flex-col gap-5 p-5">
           <h1 className="text-2xl font-bold uppercase">
             Building <br /> high-performance <br /> web applications
           </h1>
@@ -25,6 +28,8 @@ const Home = () => {
           <Button className="rounded-none">
             <p className="text-xs uppercase">Let&apos;s start building yours</p>
           </Button>
+
+          <Image src={HeroSVG} alt="" className="py-10" />
         </div>
       </main>
     </div>
