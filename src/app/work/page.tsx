47528da2239a7a04f58dfba509478022642c8081
Ryel Banfield <ryelbanfield@gmail.com>
2024-06-20 13:53:23 -0400

    Build init work page

---
+++ src/app/work/page.tsx
@@ -0,0 +1,201 @@
+const Work = () => {
+  return (
+    <main className="p-5">
+      <section className="prose prose-invert py-10 md:prose-lg min-[840px]:hidden">
+        <h2>My Work</h2>
+
+        <p>
+          On a mission to build and enhance digital experiences, I specialize in
+          creating modern, innovative websites and web applications. Here&apos;s
+          a summary of my work so far.
+        </p>
+
+        <hr />
+
+        <h3>RCB Software</h3>
+        <p>Freelance Web Developer (2023 — Present)</p>
+
+        <p>
+          At RCB Software, I collaborate with clients to elevate their digital
+          presence. I ensure solutions tailored to each client&apos;s needs.
+          Here are some of my key contributions:
+        </p>
+
+        <ul>
+          <li>
+            Implemented responsive design techniques to ensure compatibility
+            across devices.
+          </li>
+          <li>
+            Integrated third-party APIs into web applications to build
+            functionality.
+          </li>
+          <li>
+            Provided technical support and troubleshooting services when needed.
+          </li>
+          <li>
+            Maintained version control systems, such as Git, to manage changes
+            to the source code.
+          </li>
+          <li>
+            Designed websites, portals, and large-scale web applications for
+            multiple clients.
+          </li>
+        </ul>
+
+        <hr />
+
+        <h3>Contract Work</h3>
+        <p>Full Stack Developer (2021 — 2023)</p>
+
+        <p>
+          In my previous role as a contracted Full Stack Developer, I focused on
+          creating robust, scalable web applications. Here are some of my key
+          contributions:
+        </p>
+
+        <ul>
+          <li>Created unit tests for full stack web application components.</li>
+          <li>
+            Collaborated with other developers on code reviews, bug fixes, and
+            feature development.
+          </li>
+          <li>
+            Optimized the existing code base for better performance and
+            scalability.
+          </li>
+          <li>
+            Led a technical team to meet project deadlines and achieve project
+            objectives.
+          </li>
+          <li>
+            Redesigned sites to enhance navigation and improve visual appeal.
+          </li>
+        </ul>
+
+        <hr />
+
+        <h3>Mentorship and Community Contributions</h3>
+        <p>
+          Beyond my professional work, I am dedicated to giving back to the
+          developer community. I have served as a Developer Mentor, sharing my
+          knowledge and experience to guide the next generation of developers.
+          Notably, I contributed to the Republic of Trinidad and Tobago&apos;s
+          Inaugural Developer Challenge, helping to foster innovation and growth
+          in the local tech community.
+        </p>
+
+        <hr />
+
+        <h3>Personal Projects</h3>
+        <p>
+          Currently, I maintain an application I developed as the sole
+          developer. This application, which serves 50+ monthly users, showcases
+          my ability to build and manage software solutions independently.
+        </p>
+      </section>
+
+      <section className="hidden gap-10 py-10 min-[840px]:grid min-[840px]:grid-cols-2">
+        <div className="prose prose-lg prose-invert col-span-2">
+          <h2>My Work</h2>
+
+          <p>
+            On a mission to build and enhance digital experiences, I specialize
+            in creating modern, innovative websites and web applications.
+            Here&apos;s a summary of my work so far.
+          </p>
+        </div>
+
+        <div className="prose prose-lg prose-invert border-b-2 border-r-2 pb-5 pr-5">
+          <h3>RCB Software</h3>
+          <p>Freelance Web Developer (2023 — Present)</p>
+
+          <p>
+            At RCB Software, I collaborate with clients to elevate their digital
+            presence. I ensure solutions tailored to each client&apos;s needs.
+            Here are some of my key contributions:
+          </p>
+
+          <ul>
+            <li>
+              Implemented responsive design techniques to ensure compatibility
+              across devices.
+            </li>
+            <li>
+              Integrated third-party APIs into web applications to build
+              functionality.
+            </li>
+            <li>
+              Provided technical support and troubleshooting services when
+              needed.
+            </li>
+            <li>
+              Maintained version control systems, such as Git, to manage changes
+              to the source code.
+            </li>
+            <li>
+              Designed websites, portals, and large-scale web applications for
+              multiple clients.
+            </li>
+          </ul>
+        </div>
+
+        <div className="prose prose-lg prose-invert border-b-2 pb-5 pr-5">
+          <h3>Contract Work</h3>
+          <p>Full Stack Developer (2021 — 2023)</p>
+
+          <p>
+            In my previous role as a contracted Full Stack Developer, I focused
+            on creating robust, scalable web applications. Here are some of my
+            key contributions:
+          </p>
+
+          <ul>
+            <li>
+              Created unit tests for full stack web application components.
+            </li>
+            <li>
+              Collaborated with other developers on code reviews, bug fixes, and
+              feature development.
+            </li>
+            <li>
+              Optimized the existing code base for better performance and
+              scalability.
+            </li>
+            <li>
+              Led a technical team to meet project deadlines and achieve project
+              objectives.
+            </li>
+            <li>
+              Redesigned sites to enhance navigation and improve visual appeal.
+            </li>
+          </ul>
+        </div>
+
+        <div className="prose prose-lg prose-invert border-b-2 border-r-2 pb-5 pr-5">
+          <h3>Mentorship and Community Contributions</h3>
+          <p>
+            Beyond my professional work, I am dedicated to giving back to the
+            developer community. I have served as a Developer Mentor, sharing my
+            knowledge and experience to guide the next generation of developers.
+            Notably, I contributed to the Republic of Trinidad and Tobago&apos;s
+            Inaugural Developer Challenge, helping to foster innovation and
+            growth in the local tech community.
+          </p>
+        </div>
+
+        <div className="prose prose-lg prose-invert border-b-2 pb-5 pr-5">
+          <h3>Personal Projects</h3>
+          <p>
+            Currently, I maintain an application I developed as the sole
+            developer. This application, which serves 50+ monthly users,
+            showcases my ability to build and manage software solutions
+            independently.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Work;
